092019de71abd54a3092fc1583bb240e87c5f204
Mario Maia <101b147ch@gmail.com>
2018-06-03 20:23:33 -0300

    Remove deprecated deployment script.

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-
-ln -fs `realpath ./vim` ~/.vim
-echo 'source $HOME/.vim/vimrc' > ~/.vimrc
-
-ln -fs `realpath ./zshrc` ~/.zshrc
-ln -fs `realpath ./cenouro.zsh-theme` ~/.oh-my-zsh/custom/
