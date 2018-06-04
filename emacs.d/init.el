47ccd386e09e9d258cae0a50969b83de8856f836
Mario Maia <101b147ch@gmail.com>
2018-06-04 15:42:23 -0300

    Add and configure origami.el.
    * Refactor a lot;
    * Organize some sections;
    * Add some mappings and quality of life improvements.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,5 +1,7 @@
 ;;; -*- lexical-binding: t -*-
 
+;; Package 'infra' stuff.
+;{{{
 (package-initialize)
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -7,42 +9,6 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-(toggle-frame-maximized)
-
-(column-number-mode)
-(setq show-paren-delay 0)
-(show-paren-mode)
-
-(prefer-coding-system 'utf-8)
-
-;; Line numbers {{{
-(setq-default display-line-numbers-type 'relative)
-(setq-default linum-format "%3d")
-(if (fboundp 'global-display-line-numbers-mode)
-    (global-display-line-numbers-mode)
-  (global-linum-mode))
-;; }}}
-
-(setq inhibit-startup-message t)
-(setq scroll-conservatively 100)
-(setq ring-bell-function 'ignore)
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-(setq-default indent-tabs-mode nil)
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(defvar my-term-shell "/bin/zsh")
-(defadvice ansi-term (before force-bash)
-  (interactive (list my-term-shell)))
-(ad-activate 'ansi-term)
-
-(global-set-key (kbd "<C-return>") 'ansi-term)
-
-(setq package-enable-at-startup nil)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -54,21 +20,74 @@
 
 (eval-when-compile
   (require 'use-package))
+;}}}
+
+;; Sane configs.
+;{{{
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(toggle-frame-maximized)
+
+(column-number-mode)
+(show-paren-mode)
+
+(prefer-coding-system 'utf-8)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(setq inhibit-startup-message t
+      scroll-conservatively 100
+      show-paren-delay 0
+      ring-bell-function 'ignore)
+
+(setq-default indent-tabs-mode nil)
+
+(setq make-backup-files nil
+      auto-save-default nil)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+;}}}
+
+;; Line numbers config.
+;{{{
+(setq-default display-line-numbers-type 'relative
+              display-line-numbers-width 3
+              linum-format "%3d")
+(if (fboundp 'global-display-line-numbers-mode)
+    (global-display-line-numbers-mode)
+  (global-linum-mode))
+;}}}
+
+;; Mappings.
+;{{{
+(global-set-key (kbd "<C-return>") (lambda ()
+                                     (interactive)
+                                     (ansi-term "/bin/zsh")))
 
 ;; Attempt to improve ESC-ESC-ESC (keyboard-escape-quit closes splits).
 (global-set-key [escape] 'keyboard-quit)
 (global-set-key (kbd "<C-escape>") 'keyboard-escape-quit)
+;}}}
 
 ;; Packages.
+;{{{
 (setq use-package-always-ensure t)
+
+(use-package yaml-mode :defer t)
+(use-package beacon :init (beacon-mode 1))
+
+(use-package origami
+  :config
+  (add-hook 'prog-mode-hook (lambda ()
+                              (setq-local origami-fold-style 'triple-braces)
+                              (origami-mode)
+                              (origami-close-all-nodes (current-buffer)))))
 
 (use-package magit :defer t)
 (add-hook 'magit-mode-hook (lambda ()
                              (local-unset-key "j")
                              (local-set-key "k" 'magit-section-backward)
                              (local-set-key "j" 'magit-section-forward)))
-
-(use-package yaml-mode :defer t)
 
 (use-package evil
   :init
@@ -83,24 +102,15 @@
       "," 'other-window
       "o" 'delete-other-windows))
   :config
-  (evil-mode))
-
-(use-package evil-vimish-fold
-  :init
-  (use-package vimish-fold
-    :config
-    (vimish-fold-global-mode 1))
-  :config
-  (evil-vimish-fold-mode 1))
+  (evil-mode)
+  (define-key evil-insert-state-map (kbd "C-h") 'backward-delete-char-untabify))
 
 (use-package which-key
   :config
   (which-key-mode)
   (setq which-key-allow-evil-operators nil))
+;}}}
 
-(use-package beacon
-  :init
-  (beacon-mode 1))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -113,7 +123,7 @@
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (yaml-mode evil-leader evil-vimish-fold magit beacon use-package which-key spacemacs-theme evil))))
+    (origami yaml-mode evil-leader evil-vimish-fold magit beacon use-package which-key spacemacs-theme evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
