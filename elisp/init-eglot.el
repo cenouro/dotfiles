e6a8f83110ab3f7816beca4623742692ed5c0914
Mario Maia <mario.mtbr+github@outlook.com>
2025-02-11 20:53:42 -0300

    Make tempel have higher precedence than eglot

--- elisp/init-eglot.el
+++ elisp/init-eglot.el
@@ -25,5 +25,22 @@ This function is meant to be used as an advice that removes
   (advice-add #'jsonrpc-connection-receive :before
               #'my/jsonrpc-connection-receive))
 
+
+(defun my/capf-tempel-before-eglot ()
+  "Make tempel have higher precedence than eglot.
+
+The `tags-completion-at-point-function' was already there and
+thus is preserved.
+
+NOTE for myself: don't try to install and use cape because it
+does not work as intended."
+  (setq-local completion-at-point-functions
+              '(tempel-complete
+                eglot-completion-at-point
+                tags-completion-at-point-function)))
+
+(with-eval-after-load 'tempel
+  (add-hook 'eglot-managed-mode-hook #'my/capf-tempel-before-eglot))
+
 (provide 'init-eglot)
 ;;; init-eglot.el ends here
