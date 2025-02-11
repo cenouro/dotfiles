e548e8ae583d75c2e6e00f506f3318e2ae58e53e
Mario Maia <mario.mtbr+github@outlook.com>
2025-02-11 19:06:05 -0300

    Make eglot use orderless

--- elisp/init-orderless.el
+++ elisp/init-orderless.el
@@ -12,7 +12,9 @@
 
 (customize-set-variable 'completion-styles '(orderless basic))
 (customize-set-variable 'completion-category-overrides
-                        '((file (style basic partial-completion))))
+                        '((file (styles basic partial-completion))
+                          (eglot (styles orderless))
+                          (eglot-capf (styles orderless))))
 
 (provide 'init-orderless)
 ;;; init-orderless.el ends here
