2b9f05f35916f88e929d5973ff5f000c60b7caae
Mario Maia <mario.mtbr+github@outlook.com>
2025-02-11 22:28:54 -0300

    Remove tempel-complete from completion-at-point-functions
    It's bound to M-i, and does not interfere with "normal" completion any
    longer.

--- elisp/init-tempel.el
+++ elisp/init-tempel.el
@@ -9,24 +9,7 @@
   (package-install 'tempel))
 (require 'tempel)
 
-(defun tempel-setup-capf ()
-  "Add the Tempel Capf to `completion-at-point-functions'.
-
-`tempel-expand' only triggers on exact matches. Alternatively use
-`tempel-complete' if you want to see all matches, but then you
-should also configure `tempel-trigger-prefix', such that Tempel
-does not trigger too often when you don't expect it.
-
-NOTE: We add `tempel-expand' *before* the main programming mode
-Capf, such that it will be tried first."
-  (setq-local completion-at-point-functions
-              (cons #'tempel-complete
-                    completion-at-point-functions)))
-
-(add-hook 'conf-mode-hook #'tempel-setup-capf)
-(add-hook 'prog-mode-hook #'tempel-setup-capf)
-(add-hook 'text-mode-hook #'tempel-setup-capf)
-
+(define-key global-map (kbd "M-i") #'tempel-complete)
 (define-key tempel-map (kbd "M-n") #'tempel-next)
 (define-key tempel-map (kbd "M-p") #'tempel-previous)
 
