bd80ef51e89a385cb40e78e79e3b931912120d0e
Mario Maia <mario.mtbr+github@outlook.com>
2025-07-08 20:25:15 -0300

    Remove excessive white-spaces

--- elisp/init-custom.el
+++ elisp/init-custom.el
@@ -36,11 +36,9 @@ to `customize-set-variable'.
 
 (defun my/add-to-list (comment list-var element &optional append compare-fn)
   "My decorated `add-to-list'.
-
 Like `add-to-list', but actually uses `customize-set-variable'.
 
 COMMENT is passed to `customize-set-variable'.
-
 LIST-VAR, ELEMENT, APPEND and COMPARE-FN are passed to `add-to-list'."
   (customize-set-variable list-var
                           (add-to-list list-var element append compare-fn)
