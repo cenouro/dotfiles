cenouro/dotfiles

commit ed17348b6b45fceb53d1d9592154e5546ba5dc2a

Move configurations from init-prog.el to init.el

init.el
@@ -128,6 +128,21 @@
 (require 'init-magit)
 
 
+;;;; Programming Languages
+;;
+(use-package prog-mode
+  :ensure nil
+  :init
+  (add-hook 'prog-mode-hook #'flyspell-prog-mode)
+  (add-hook 'prog-mode-hook #'(lambda () (setq truncate-lines t))))
+
+(use-package hl-todo
+  :vc "https://github.com/tarsius/hl-todo"
+  :demand t
+  :config (global-hl-todo-mode 1))
+
+
+
 (require 'init-emacs)
 (require 'init-imenu)
 
@@ -137,7 +152,6 @@
 (require 'init-project)
 (require 'init-flymake)
 (require 'init-eglot)
-(require 'init-prog)
 (require 'init-apache)
 (require 'init-markdown)
 (require 'init-yaml)

elisp/init-apache.el
@@ -4,7 +4,6 @@
 
 ;;; Code:
 (require 'init-package)
-(require 'init-prog)
 
 (unless (package-installed-p 'apache-mode)
   (package-install 'apache-mode))

elisp/init-markdown.el
@@ -4,7 +4,6 @@
 
 ;;; Code:
 (require 'init-package)
-(require 'init-prog)
 
 (unless (package-installed-p 'markdown-mode)
   (package-install 'markdown-mode))

elisp/init-prog.el
@@ -1,22 +1,0 @@
-;;; init-prog.el ---  -*- lexical-binding: t; -*-
-
-;;; Commentary:
-;;
-;; Configurations, packages and settings that are useful for more than
-;; a single programming language.
-
-;;; Code:
-(require 'package)
-(unless (package-installed-p 'hl-todo)
-  (package-vc-install (github "tarsius/hl-todo")))
-
-
-(add-hook 'prog-mode-hook #'flyspell-prog-mode)
-(add-hook 'prog-mode-hook #'(lambda () (setq truncate-lines t)))
-
-
-(require 'hl-todo)
-(global-hl-todo-mode)
-
-(provide 'init-prog)
-;;; init-prog.el ends here

elisp/init-yaml.el
@@ -4,7 +4,6 @@
 
 ;;; Code:
 (require 'init-package)
-(require 'init-prog)
 
 (unless (package-installed-p 'yaml-mode)
   (package-install 'yaml-mode))

commit e017ea68c12907b64588118fe580a1cf9acb6276

Fix bug introduced by ed17348b6b45fceb53d1d9592154e5546ba5dc2a
The documentation for use-package states that the :vc keyword accepts
all arguments that package-vc-install accepts, but that is not true.
While package-vc-install accepts an URL string, :vc requires the
form (:url "url-string") to be used.

init.el
@@ -137,7 +137,7 @@
   (add-hook 'prog-mode-hook #'(lambda () (setq truncate-lines t))))
 
 (use-package hl-todo
-  :vc "https://github.com/tarsius/hl-todo"
+  :vc (:url "https://github.com/tarsius/hl-todo")
   :demand t
   :config (global-hl-todo-mode 1))
 