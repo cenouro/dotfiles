cenouro/dotfiles

commit 35f9a720b438f7acd567e28fe0b9e52901d10522

Move configurations from init-git.el to init.el
The file init-magit.el is now required in the new init.el's section
"Version Control and Diffs", which improves organization a little bit.

init.el
@@ -99,7 +99,22 @@
          ("M-n" . #'tempel-next)
          ("M-p" . #'tempel-previous)))
 
+
+;;;; Version Control and Diffs
+;;
+(use-package vc-git
+  :custom (vc-git-diff-switches "--patience"))
 
+(use-package ediff
+  :custom
+  ((ediff-window-setup-function
+    #'ediff-setup-windows-plain "Don't use a separate frame")
+   (ediff-split-window-function
+    #'split-window-horizontally "Diff side-by-side")))
+
+(require 'init-magit)
+
+
 (require 'init-emacs)
 (require 'init-imenu)
 
@@ -119,8 +134,6 @@
 (require 'init-yaml)
 (require 'init-asdf)
 (require 'init-elisp)
-(require 'init-git)
-(require 'init-magit)
 (require 'init-ruby)
 
 

elisp/init-git.el
@@ -1,23 +1,0 @@
-;;; init-git.el ---  -*- lexical-binding: t; -*-
-
-;;; Commentary:
-;;
-
-
-;;; Code:
-(with-eval-after-load 'vc-git
-  (customize-set-variable 'vc-git-diff-switches "--patience"))
-
-
-(with-eval-after-load 'ediff
-  (customize-set-variable 'ediff-window-setup-function
-                          'ediff-setup-windows-plain
-                          "Don't use a separate frame")
-  (customize-set-variable 'ediff-split-window-function
-                          'split-window-horizontally
-                          "Diff side-by-side"))
-
-
-(provide 'init-git)
-
-;;; init-git.el ends here

commit 2b7db5768ae20394d144eb81055f0f62d933167d

Fix bug introduced by 35f9a720b438f7acd567e28fe0b9e52901d10522
Although vc-git is a feature (meaning it can be required), it is not a
package, thus it can't be installed with package-install. I've
configured use-package to always ensure packages, therefore an error
occurred because use-package was indeed attempting to install vc-git.

An alternative solution would be to use:

(use-pacakge vc-git
  :ensure vc
  ...)

This way, use-package would supposedly ensure the package vc, and
load the feature with (require 'vc-git).

Anyway, I like to use magit, and my usage of vc-git has been minimal if
not nonexistent, therefore I chose a simpler solution.

init.el
@@ -107,7 +107,7 @@
 
 ;;;; Version Control and Diffs
 ;;
-(use-package vc-git
+(use-package vc
   :custom (vc-git-diff-switches "--patience"))
 
 (use-package ediff